157b3ea4c1e3ce1632c506376078feb4aecd63de
hippieperm <leess0133@gmail.com>
2025-04-16 20:48:42 +0900

    홈 페이지에 배경색을 추가하고, 날씨 정보를 표시하는 리스트 뷰를 구현함

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -10,6 +10,7 @@ class HomePage extends StatelessWidget {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
+        backgroundColor: Colors.white.withAlpha(255),
         appBar: AppBar(
           title: TextField(
             decoration: InputDecoration(
@@ -25,7 +26,42 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ),
-        body: Center(child: Text('Home')),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: ListView.separated(
+            itemBuilder: (context, index) {
+              return Container(
+                height: 120,
+                color: const Color.fromARGB(255, 216, 211, 211).withAlpha(110),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            '오늘의 날씨',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text('오늘의 날씨', style: TextStyle(fontSize: 14)),
+                          Text('오늘의 날씨', style: TextStyle(fontSize: 14)),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return SizedBox(height: 10);
+            },
+            itemCount: 10,
+          ),
+        ),
       ),
     );
   }
