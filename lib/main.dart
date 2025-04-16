c29dc24be2f0f854b2317a41bbca760940c37f95
hippieperm <leess0133@gmail.com>
2025-04-16 20:27:17 +0900

    홈 페이지를 추가하고 기본 앱 구조를 수정하여 MaterialApp의 홈 속성을 HomePage로 설정함

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_location_explorer/ui/pages/home/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(home: HomePage());
   }
 }
