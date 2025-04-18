88b6d54fe303c808b1c1e95dd888069bb7eee57c
hippieperm <leess0133@gmail.com>
2025-04-18 16:17:15 +0900

    홈 페이지의 구조를 단순화하고, 기존의 리스트 아이템을 제거하여 기본적인 스캐폴드를 설정함. 앱바 제목을 'Location Explorer'로 변경하고, 본문에 플레이스홀더 추가.

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,50 +1,13 @@
 import 'package:flutter/material.dart';
-import 'widget/list_item.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).unfocus();
-      },
-      child: Scaffold(
-        backgroundColor: Colors.white.withAlpha(255),
-        appBar: AppBar(
-          title: TextField(
-            decoration: InputDecoration(
-              hintText: '위치입력',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-                borderSide: const BorderSide(color: Colors.grey),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-                borderSide: const BorderSide(color: Colors.grey),
-              ),
-            ),
-          ),
-        ),
-
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: ListView.separated(
-            itemBuilder: (context, index) {
-              return const ListItem(
-                title: '삼성1동 주민센터',
-                category: '공공, 사회기관>행복복지센터',
-                roadAddress: '서울특별시 강남구 봉은사로 616 삼성1동 주민센터',
-              );
-            },
-            separatorBuilder: (context, index) {
-              return const SizedBox(height: 5);
-            },
-            itemCount: 10,
-          ),
-        ),
-      ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Location Explorer')),
+      body: const Placeholder(),
     );
   }
 }
