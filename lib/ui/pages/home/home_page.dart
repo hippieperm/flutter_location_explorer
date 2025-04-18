d6db64f1b0eab4c5a81ab9982c48e4907407e2dd
hippieperm <leess0133@gmail.com>
2025-04-18 16:24:22 +0900

    홈 페이지의 앱바 배경색을 변경하고, 본문에 안전 영역을 추가하여 배경을 그라데이션으로 설정하는 기능을 구현함.

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -6,8 +6,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Location Explorer')),
-      body: const Placeholder(),
+      appBar: AppBar(backgroundColor: Colors.blue[700]),
+      body: SafeArea(child: Stack(children: [_buildBackground()])),
     );
   }
+}
+
+Widget _buildBackground() {
+  return Container(
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [Colors.blue[700]!, Colors.blue[500]!, Colors.blue[300]!],
+      ),
+    ),
+  );
 }
