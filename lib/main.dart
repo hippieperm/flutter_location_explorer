efc67abcdc461fb33c939b54e4547ebc117fc146
hippieperm <leess0133@gmail.com>
2025-04-18 16:17:03 +0900

    앱 초기화 시 환경 변수를 로드하고, ChangeNotifierProvider를 사용하여 PlaceProvider를 추가함으로써 상태 관리를 개선함. MaterialApp의 테마와 홈 페이지 설정을 업데이트함.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_location_explorer/data/provider/place_provider.dart';
+import 'package:provider/provider.dart';
 import 'ui/pages/home/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return ChangeNotifierProvider(
+      create: (context) => PlaceProvider(),
+      child: MaterialApp(
+        title: 'Location Explorer',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          visualDensity:
+              VisualDensity.adaptivePlatformDensity, // 모바일 환경에 맞게 레이아웃 조정
+          useMaterial3: true, // 머티리얼 3 테마 사용
+        ),
+        home: const HomePage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
   }
 }
