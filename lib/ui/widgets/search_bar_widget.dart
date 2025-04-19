18995c404c820a9f491dda6357cc49adf923e371
hippieperm <leess0133@gmail.com>
2025-04-20 06:48:55 +0900

    검색 페이지에 새로운 검색 바 위젯을 추가하여 사용자 인터페이스를 개선하고, 검색어 입력 기능을 구현함.

---
+++ lib/ui/widgets/search_bar_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class SearchBarWidget extends StatelessWidget {
+  const SearchBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(18),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withAlpha(26),
+            blurRadius: 8,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Icon(Icons.search, color: Colors.grey[600]),
+          SizedBox(width: 8),
+          Expanded(
+            child: TextField(
+              controller: TextEditingController(),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: '검색어를 입력하세요',
+                hintStyle: TextStyle(color: Colors.grey[400]),
+              ),
+              onSubmitted: (value) {
+                if (value.isNotEmpty) {}
+              },
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.clear, color: Colors.grey),
+            onPressed: () {},
+          ),
+          IconButton(icon: const Icon(Icons.search), onPressed: () {}),
+        ],
+      ),
+    );
+  }
+}
