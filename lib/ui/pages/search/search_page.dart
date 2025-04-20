ca509e53cd88971ac1dbcf89c266c74e97673a0a
hippieperm <leess0133@gmail.com>
2025-04-21 04:50:46 +0900

    검색 페이지를 StatelessWidget에서 StatefulWidget으로 변경하여 상태 관리를 추가하고, 검색 페이지의 상태를 관리할 수 있는 기능을 구현함.

--- lib/ui/pages/search/search_page.dart
+++ lib/ui/pages/search/search_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_location_explorer/ui/widgets/search_bar_widget.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
