121072b3985a1fa070a00875334f0f06808f8504
hippieperm <leess0133@gmail.com>
2025-04-21 05:56:32 +0900

    PlaceProvider 클래스에 현재 위치 가져오기 및 키워드로 장소 검색 기능을 추가하고, 검색 결과 업데이트 및 초기화 메서드를 구현함.

--- lib/data/provider/place_provider.dart
+++ lib/data/provider/place_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_location_explorer/data/model/place.dart';
 import 'package:flutter_location_explorer/data/repository/place_repository.dart';
+import 'package:flutter_location_explorer/util/location_util.dart';
 import 'package:geolocator/geolocator.dart';
 
 enum SearchLoadingState { initial, loading, loaded, error }
@@ -27,4 +28,60 @@ class PlaceProvider extends ChangeNotifier {
   int get totalItems => _totalItems;
   int get currentPage => _currentPage;
   bool get hasMoreItems => _hasMoreItems;
+
+  // 검색 결과 업데이트
+  void _updateSearchResults(PlaceResponse response) {
+    _places = response.items;
+    _totalItems = response.total;
+    _hasMoreItems = _places.length < response.total;
+    _loadingState = SearchLoadingState.loaded;
+  }
+
+  // 검색 초기화
+  void resetSearch() {
+    _loadingState = SearchLoadingState.initial;
+    _places = [];
+    _searchQuery = '';
+    _currentPage = 1;
+    _hasMoreItems = true;
+    notifyListeners();
+  }
+
+  Future<void> getCurrentLocation() async {
+    _loadingState = SearchLoadingState.loading;
+    notifyListeners();
+
+    try {
+      _currentPosition = await LocationUtil.getCurrentLocation();
+      _loadingState = SearchLoadingState.loaded;
+    } catch (e) {
+      _loadingState = SearchLoadingState.error;
+      _errorMessage = '위치를 가져오는 데 실패했습니다.';
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> searchPlacesByKeyword(String query) async {
+    _loadingState = SearchLoadingState.loading;
+    _searchQuery = query;
+    _currentPage = 1;
+    _places = [];
+    notifyListeners();
+
+    try {
+      final response = await _repository.searchPlacesByKeyword(
+        query: query,
+        display: _itemsPerPage,
+        start: 1,
+      );
+
+      _updateSearchResults(response);
+    } catch (e) {
+      _loadingState = SearchLoadingState.error;
+      _errorMessage = '검색 중 오류가 발생했습니다.';
+    }
+
+    notifyListeners();
+  }
 }
