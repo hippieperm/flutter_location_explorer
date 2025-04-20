ae0f3f0c4bc99653e5dac65432b1a0003350aadf
hippieperm <leess0133@gmail.com>
2025-04-21 05:44:13 +0900

    네이버 API 클라이언트에 장소 검색 기능을 추가하여 쿼리, 표시 수, 시작 위치 및 정렬 방식을 매개변수로 받아 API 호출을 통해 장소 정보를 검색할 수 있도록 구현함.

--- lib/data/repository/naver_api_client.dart
+++ lib/data/repository/naver_api_client.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_location_explorer/data/model/place.dart';
 
 class NaverApiClient {
   final Dio _dio = Dio();
@@ -16,5 +17,26 @@ class NaverApiClient {
       'X-Naver-Client-Id': dotenv.env['NAVER_CLIENT_ID'] ?? '',
       'X-Naver-Client-Secret': dotenv.env['NAVER_CLIENT_SECRET'] ?? '',
     };
+  }
+  Future<PlaceResponse> searchPlaces({
+    required String query,
+    int display = 10,
+    int start = 1,
+    String sort = 'random', // random, comment
+  }) async {
+    try {
+      final response = await _dio.get(
+        '$_baseUrl/local.json',
+        queryParameters: {
+          'query': query,
+          'display': display,
+          'start': start,
+          'sort': sort,
+        },
+      );
+      return PlaceResponse.fromJson(response.data);
+    } catch (e) {
+      return PlaceResponse(total: 0, start: 0, display: 0, items: []);
+    }
   }
 }
