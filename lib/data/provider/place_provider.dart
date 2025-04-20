67db926c69ea892a096766e1724c36fad0b08c95
hippieperm <leess0133@gmail.com>
2025-04-21 05:53:46 +0900

    PlaceProvider 클래스에 상태 변수 및 메서드를 추가하여 장소 검색 기능의 상태 관리를 구현함. 검색 쿼리, 현재 위치, 페이지 정보 등을 포함한 상태 변수를 정의하고, 해당 변수에 대한 getter 메서드를 추가함.

--- lib/data/provider/place_provider.dart
+++ lib/data/provider/place_provider.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_location_explorer/data/model/place.dart';
+import 'package:flutter_location_explorer/data/repository/place_repository.dart';
+import 'package:geolocator/geolocator.dart';
 
 enum SearchLoadingState { initial, loading, loaded, error }
 
 class PlaceProvider extends ChangeNotifier {
+  final PlaceRepository _repository = PlaceRepository();
 
+  // 상태 변수
+  SearchLoadingState _loadingState = SearchLoadingState.initial;
+  String _errorMessage = '';
+  List<Place> _places = [];
+  Position? _currentPosition;
+  String _searchQuery = '';
+  int _totalItems = 0;
+  int _currentPage = 1;
+  int _itemsPerPage = 10;
+  bool _hasMoreItems = true;
+
+  SearchLoadingState get loadingState => _loadingState;
+  String get errorMessage => _errorMessage;
+  List<Place> get places => _places;
+  Position? get currentPosition => _currentPosition;
+  String get searchQuery => _searchQuery;
+  int get totalItems => _totalItems;
+  int get currentPage => _currentPage;
+  bool get hasMoreItems => _hasMoreItems;
 }
