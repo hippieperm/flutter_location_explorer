ae92e4194bdc7926accc88184d6155f838fc3946
hippieperm <leess0133@gmail.com>
2025-04-21 05:47:18 +0900

    장소 검색 기능을 위한 PlaceRepository 클래스를 추가하고, 위치 기반 검색을 위한 메서드를 구현함. 네이버 API 클라이언트를 통해 장소 정보를 검색할 수 있도록 설정함.

---
+++ lib/data/repository/place_repository.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_location_explorer/data/model/place.dart';
+import 'package:flutter_location_explorer/data/repository/naver_api_client.dart';
+
+class PlaceRepository {
+  final NaverApiClient _naverApiClient = NaverApiClient();
+
+  Future<PlaceResponse> searchPlacesByLocation({
+    required String query,
+    required double latitude,
+    required double longitude,
+    int radius = 5000, // 기본 반경 5km
+    int display = 10,
+    int start = 1,
+    String sort = 'random',
+  }) async {
+    final locationQuery = '$query 근처';
+
+    return await _naverApiClient.searchPlaces(
+      query: locationQuery,
+      display: display,
+      start: start,
+      sort: sort,
+    );
+  }
+}
