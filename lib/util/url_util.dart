92b15172a5d075b42642b30f73a25001f7813d4b
hippieperm <leess0133@gmail.com>
2025-04-21 14:49:56 +0900

    URL 유틸리티 클래스에 HTML 태그를 제거하는 기능을 추가하고, Place 객체를 사용하여 지도 열기 메서드를 개선함.

--- lib/util/url_util.dart
+++ lib/util/url_util.dart
@@ -1,4 +1,5 @@
 import 'package:url_launcher/url_launcher.dart';
+import '../data/model/place.dart';
 
 class UrlUtil {
   // URL 열기
@@ -26,9 +27,27 @@ class UrlUtil {
 
   // 지도 열기 - 수정된 버전
   static Future<bool> openMap(double mapx, double mapy, String title) async {
+    // HTML 태그 제거
+    final cleanTitle = title.replaceAll('<b>', '').replaceAll('</b>', '');
+
     // 네이버 지도는 epsg:5179 좌표를 사용하므로,
     // 직접 네이버 검색 결과 페이지로 이동하는 방식으로 변경
-    final encodedTitle = Uri.encodeComponent(title);
+    final encodedTitle = Uri.encodeComponent(cleanTitle);
+    final url =
+        'https://m.map.naver.com/search2/search.naver?query=$encodedTitle';
+
+    final Uri uri = Uri.parse(url);
+
+    if (await canLaunchUrl(uri)) {
+      return await launchUrl(uri, mode: LaunchMode.externalApplication);
+    }
+
+    return false;
+  }
+
+  // Place 객체를 사용하여 지도 열기 (더 나은 방식)
+  static Future<bool> openMapWithPlace(Place place) async {
+    final encodedTitle = Uri.encodeComponent(place.cleanTitle);
     final url =
         'https://m.map.naver.com/search2/search.naver?query=$encodedTitle';
 
